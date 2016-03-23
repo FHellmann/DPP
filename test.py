b8f0efb97b4586a83fdcbfabd94d9522c87945dc
mheimbuc <mheimbuc@hm.edu>
2016-03-23 12:10:12 +0100

    [Tester] Better error message Counts forks too now

--- test.py
+++ test.py
@@ -20,9 +20,12 @@ def main():
         forks = set()
         chairs = set()
 
+        linecounter = 1
         for line in f:
             chairfound = re.compile('Found a nice seat \\(Chair\\-([0-9]*)\\)')
             chairleave = re.compile('Stand up from seat \\(Chair\\-([0-9]*)\\)')
+            forktake = re.compile('Picked up fork \\(Fork\\-([0-9]*)\\ from Chair\\-([0-9]*)\\)')
+            forkput = re.compile('Release my fork \\(Fork\\-([0-9]*)\\ from Chair\\-([0-9]*)\\)')
 
 
             l = parse_line(line)
@@ -31,9 +34,9 @@ def main():
 
             if 'Found a nice seat' in msg:
                 mat = re.match(chairfound, msg)
-                
+
                 if mat.group(1) in chairs:
-                    print('Chair', mat.group(1), 'already occupied')
+                    print('Chair', mat.group(1), 'already occupied in line', linecounter)
                     sys.exit(1)
 
                 chairs.add(mat.group(1))
@@ -41,11 +44,29 @@ def main():
                 mat = re.match(chairleave, msg)
 
                 if mat.group(1) not in chairs:
-                    print('Chair', mat.group(1), 'not occupied')
+                    print('Chair', mat.group(1), 'not occupied in line', linecounter)
                     sys.exit(1)
 
                 chairs.remove(mat.group(1))
 
+            elif 'Picked up fork' in msg:
+                mat = re.match(forktake, msg)
+
+                if mat.group(1) in forks:
+                    print('Fork', mat.group(1), 'already occupied in line', linecounter)
+                    sys.exit(1)
+
+                forks.add(mat.group(1))
+            elif 'Release my fork' in msg:
+                mat = re.match(forkput, msg)
+
+                if mat.group(1) not in forks:
+                    print('Fork', mat.group(1), 'not occupied in line', linecounter)
+                    sys.exit(1)
+
+                forks.remove(mat.group(1))
+
+            linecounter += 1
 
 if __name__ == '__main__':
     main()
